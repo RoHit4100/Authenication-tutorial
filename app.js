d6aa1d500734f71dd7824f8fa7d90bd91c7cb374
RoHit4100 <rohitudamale777@gmail.com>
2023-01-02 14:22:42 +0530

    Interface is created with ejs

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 main().catch(err => console.log(err));
 async function main() {
     mongoose.set("strictQuery", true);
     await mongoose.connect("mongodb://127.0.0.1:27017/authDb")
 }
 
+app.get("/", function (req, res) {
+    res.render("home.ejs")
+})
 app.listen(port, function () {
     console.log("Server is running on port " + port);
 })
