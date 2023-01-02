53e45288331902e0f965253fa43260e5dc449e31
RoHit4100 <rohitudamale777@gmail.com>
2023-01-02 14:49:31 +0530

    level 1 auth is done

--- app.js
+++ app.js
@@ -12,8 +12,58 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/authDb")
 }
 
+const userSchema = new mongoose.Schema({
+    userName: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
 app.get("/", function (req, res) {
     res.render("home.ejs")
+})
+
+app.route("/register")
+    .get(function (req, res) {
+        res.render("register.ejs")
+    })
+    .post(function (req, res) {
+        const user = new User({
+            userName: req.body.userName,
+            password: req.body.password
+        });
+        user.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets.ejs")
+            }
+        });
+    })
+app.route("/login")
+    .get(function (req, res) {
+        res.render("login.ejs")
+    })
+    .post(function (req, res) {
+        const userName = (req.body.userName).trim();
+        const password = (req.body.password).trim();
+        User.findOne({ userName: userName }, function (err, foundUser) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUser) {
+                    if (foundUser.password === password) {
+                        res.render("secrets.ejs");
+                    } else {
+                        alert("Please enter the valid details")
+                    }
+                }
+            }
+        })
+    })
+
+
+app.get("/logout", function (req, res) {
+    res.render("home.ejs");
 })
 app.listen(port, function () {
     console.log("Server is running on port " + port);
